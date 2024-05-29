534f050fd12ebb788acd369e4c56afb2026f9e9a
Adson Souza <ty.msn@hotmail.com>
2024-05-29 17:30:20 -0300

    Organize and create interface repository

--- src/tests/services/product.service.spec.ts
+++ src/tests/services/product.service.spec.ts
@@ -1,4 +1,4 @@
-import Product from "../../entity/product"
+import Product from "../../domain/entity/product"
 import ProductService from "../../service/product.service"
 
 describe("ProductService unit",()=>{
