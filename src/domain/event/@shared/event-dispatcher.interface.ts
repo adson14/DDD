e8fcbef0a60533bbdb5aa837ec7849afc993bf00
Adson Souza <ty.msn@hotmail.com>
2024-06-01 12:35:51 -0300

    Create interfaces events

---
+++ src/domain/event/@shared/event-dispatcher.interface.ts
@@ -0,0 +1,13 @@
+import EventHandlerInterface from "./event-handler.interface";
+import EventInterface from "./event.interface";
+
+export default interface EventDispatcherInterface {
+
+  notify(event: EventInterface): void
+
+  register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void
+
+  unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void
+
+  unregisterAll(): void
+}
